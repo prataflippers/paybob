b55ce311820d2a310d4b875e1b82c8329d5752b7
Shi Tian <shitian007@hotmail.com>
2017-10-14 21:35:02 +0800

    Add handler templates for all actions

--- controllers/parse_message.py
+++ controllers/parse_message.py
@@ -1,18 +1,30 @@
-def parse_handler(message):
+from acknowledge_handler import acknowledge_handler
+from debt_handler import debt_handler
+from history_handler import history_handler
+from loan_handler import loan_handler
+from pay_handler import pay_handler
+from retract_handler import retract_handler
+
+def parse_handler(user_id, message):
     '''
     Passes on the user request to the specific controller
     ARGUMENTS:
     message: string
     RETURN TYPE: void
     '''
-    text = message["text"]
-    command = text.split(' ')[0][1:]
-    argument = text.split(' ', 1)[1] if len(text.split(' ', 1))>1 else None
-    if command == "add":
-        print("Add handler")
-    elif command == "show":
-        print("Show handler")
-    elif command == "clear":
-        print("Clear handler")
+    command = message.split(' ')[0][1:]
+    argument = message.split(' ', 1)[1] if len(message.split(' ', 1)) > 1 else None
+    if command == "acknowledge":
+        acknowledge_handler(user_id, argument)
+    elif command == "debts":
+        debt_handler(user_id, argument)
+    elif command == "history":
+        history_handler(user_id, argument)
+    elif command == "loans":
+        loan_handler(user_id, argument)
+    elif command == "pay":
+        pay_handler(user_id, argument)
+    elif command == "retract":
+        retract_handler(user_id, argument)
     else:
         print("Invalid command")
---
+++ controllers/pay_handler.py
@@ -0,0 +1,3 @@
+USAGE_MESSAGE = "`/pay <user> <amount>` to pay user the stipulated amount"
+
+def pay_handler(user_id, other_user, amount):
