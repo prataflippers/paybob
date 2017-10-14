49975f9153c95e6dec77bced2b3f656508e3afb0
Haozhe <phaozhe@hotmail.com>
2017-10-14 16:29:53 +0800

    added timeout value

--- echobot.py
+++ echobot.py
@@ -18,10 +18,27 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates():
-    url = URL + "getUpdates"
+def get_updates(offset=None):
+    url = URL + "getUpdates?timeout=100"
+    if offset:
+        url += "&offset={}".format(offset)
     js = get_json_from_url(url)
     return js
+
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
 
 
 def get_last_chat_id_and_text(updates):
@@ -36,6 +53,15 @@ def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message("delicious", "197340571")
+
+if __name__ == '__main__':
+    main()
