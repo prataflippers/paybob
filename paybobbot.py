30a7b02b17c7fc57b14a4c4c95cbe1a757bcc07f
Shi Tian <shitian007@hotmail.com>
2017-10-14 20:32:17 +0800

    Modify parser for telepot

--- paybobbot.py
+++ paybobbot.py
@@ -1,18 +1,23 @@
+import time
 import telepot
 from telepot.loop import MessageLoop
+import controllers.parse_message as parser
 
 # Initial setup
 paybot = telepot.Bot("452146569:AAEdRQMubxBqRpSWYFs931wnUFja8vdHIIQ")
 paybot.getUpdates(offset=100)
 
-def handler():
+def handler(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
     print(content_type, chat_type, chat_id)
 
     if content_type == 'text':
         paybot.sendMessage(chat_id, msg['text'])
+        print(msg)
+        parser.parse_handler(msg)
 
 # Run loop
 MessageLoop(paybot, handler).run_as_thread()
 
-
+while 1:
+    time.sleep(100)
