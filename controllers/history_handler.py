5ddf19fb6e84f43bf45c2b4820766ebea454fc34
Shi Tian <shitian007@hotmail.com>
2017-10-14 23:07:25 +0800

    Add print to handlers

--- controllers/history_handler.py
+++ controllers/history_handler.py
@@ -1,3 +1,4 @@
 USAGE_MESSAGE = "`/history` to display previous transactions"
 
 def history_handler(user_id, other_user, amount):
+    print("History handler")
--- controllers/retract_handler.py
+++ controllers/retract_handler.py
@@ -1,3 +1,4 @@
 USAGE_MESSAGE = "`/retract <user> <amount>` to retract an unacknowledged payment"
 
 def retract_handler(user_id, other_user, amount):
+    rint("Pay Handler")
