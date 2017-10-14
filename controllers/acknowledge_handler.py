0d5e159845ac4ea0ca1b362522b514089f9f39df
Shi Tian <shitian007@hotmail.com>
2017-10-15 03:12:33 +0800

    Update acknowledge handler

--- controllers/acknowledge_handler.py
+++ controllers/acknowledge_handler.py
@@ -1,4 +1,28 @@
-USAGE_MESSAGE = "`/acknowledge <user>` to acknowledge payment from user, `/acknowledge` to acknowledge all incoming payments"
+import Database
+import telepot
+
+# Messages
+USAGE_MESSAGE = "Usage: `/acknowledge <user>` to acknowledge payment from user, `/acknowledge` to acknowledge all incoming payments"
+USER_NOT_FOUND = "Either specified user does not exist or is currently not using the bot. Please request for him/her to add @paybob"
 
 def acknowledge_handler(user_id, arguments):
-    print("Acknowledge Handler")
+    # Initialize bot and database helpers
+    paybot = telepot.Bot("452146569:AAEdRQMubxBqRpSWYFs931wnUFja8vdHIIQ")
+    db = Database.Database()
+
+    # Handle acknowledgement
+    payee = arguments[0]
+    amount = arguments[1]
+    try:
+        if (len(arguments) < 2):
+            paybot.sendMessage(user_id, USAGE_MESSAGE)
+        elif (db.getChatID(payee) == None):
+            paybot.sendMessage(user_id, USER_NOT_FOUND)
+        elif(float(amount)):
+            payee_id = db.getChatID(payee)
+            paybot.sendMessage(user_id, "Successfully acknowledged payment of %s from %s" % (amount, payee))
+            paybot.sendMessage(payee_id, "Payment of %s to %s acknowledged", (db.getUsername(user_id), amount))
+        else:
+            paybot.sendMessage(user_id, USAGE_MESSAGE)
+    except:
+        paybot.sendMessage(user_id, USAGE_MESSAGE)
