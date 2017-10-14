3faed883f8276bcc213a1df6a805c65942f5517a
Shi Tian <shitian007@hotmail.com>
2017-10-14 17:10:46 +0800

    Add basic documentation

--- echobot.py
+++ echobot.py
@@ -8,18 +8,34 @@ URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
 
 def get_url(url):
+    '''
+    ARGUMENTS:
+    url: String
+    RETURN TYPE: Response Object
+    '''
     response = requests.get(url)
     content = response.content.decode("utf8")
     return content
 
 
 def get_json_from_url(url):
+    '''
+    ARGUMENTS:
+    url: String
+    RETURN TYPE: JSON Object
+    '''
     content = get_url(url)
     js = json.loads(content)
     return js
 
 
 def get_updates(offset=None):
+    '''
+    Long polling for updates
+    ARGUMENTS:
+    offset: Integer
+    RETURN TYPE: JSON Object
+    '''
     url = URL + "getUpdates?timeout=100"
     if offset:
         url += "&offset={}".format(offset)
@@ -27,12 +43,23 @@ def get_updates(offset=None):
     return js
 
 def get_last_update_id(updates):
+    '''
+    ARGUMENTS:
+    updates: Array<JSON>
+    RETURN TYPE: Integer chat ID
+    '''
     update_ids = []
     for update in updates["result"]:
         update_ids.append(int(update["update_id"]))
     return max(update_ids)
 
 def echo_all(updates):
+    '''
+    Echoes all text messages within updates
+    ARGUMENTS:
+    updates: Array<JSON>
+    RETURN TYPE: Void
+    '''
     for update in updates["result"]:
         try:
             text = update["message"]["text"]
@@ -43,6 +70,12 @@ def echo_all(updates):
 
 
 def get_last_chat_id_and_text(updates):
+    '''
+    Get last chat id and text
+    ARGUMENTS:
+    updates: Array<JSON>
+    RETURN TYPE: Tuple(String text, Integer ID)
+    '''
     num_updates = len(updates["result"])
     last_update = num_updates - 1
     text = updates["result"][last_update]["message"]["text"]
@@ -51,6 +84,12 @@ def get_last_chat_id_and_text(updates):
 
 
 def send_message(text, chat_id):
+    '''
+    ARGUMENTS:
+    text: String
+    chat_id: Integer
+    RETURN TYPE: Void
+    '''
     text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
