5dcb2ba6becaf6b38202a43022f65a936bfb6020
Suyash Shekhar <suyashshekhar@gmail.com>
2017-10-16 22:48:04 +0800

    Notify errors to admins

--- controllers/parse_message.py
+++ controllers/parse_message.py
@@ -10,6 +10,8 @@ import telepot
 import Database
 
 db = Database.Database()
+paybot = telepot.Bot("452146569:AAFd8H6aj0ifJIpVT_zfxlSad8WOBUSjU2c")
+
 
 def parse_handler(user_id, username, message):
     '''
@@ -19,31 +21,44 @@ def parse_handler(user_id, username, message):
     RETURN TYPE: void
     '''
     # Initialisation of bot
-    paybot = telepot.Bot("452146569:AAFd8H6aj0ifJIpVT_zfxlSad8WOBUSjU2c")
     command = message.split(' ')[0][1:]
     arguments = message.split(' ')[1:] if len(message.split(' ')) > 1 else None
 
-    if db.userExists(user_id):
-        if command == "start":
-            start_handler(user_id, arguments)
-        elif command == "acknowledge":
-            acknowledge_handler(user_id, arguments)
-        elif command == "add":
-            add_handler(user_id, arguments)
-        elif command == "debts":
-            debt_handler(user_id, arguments)
-        elif command == "history":
-            history_handler(user_id, arguments)
-        elif command == "loans":
-            loan_handler(user_id, arguments)
-        elif command == "give":
-            give_handler(user_id, arguments)
-        elif command == "retract":
-            retract_handler(user_id, arguments)
+    try:
+        if db.userExists(user_id):
+            if command == "start":
+                start_handler(user_id, arguments)
+            elif command == "acknowledge":
+                acknowledge_handler(user_id, arguments)
+            elif command == "add":
+                add_handler(user_id, arguments)
+            elif command == "debts":
+                debt_handler(user_id, arguments)
+            elif command == "history":
+                history_handler(user_id, arguments)
+            elif command == "loans":
+                loan_handler(user_id, arguments)
+            elif command == "give":
+                give_handler(user_id, arguments)
+            elif command == "retract":
+                retract_handler(user_id, arguments)
+            else:
+                paybot.sendMessage(user_id, "Invalid Command")
         else:
-            paybot.sendMessage(user_id, "Invalid Command")
-    else:
-        if command == "start":
-            start_handler(user_id, username, arguments)
-        else:
-            print("POTENTIAL SQL INJECTION THREAT FROM {}!".format(user_id))
+            if command == "start":
+                start_handler(user_id, username, arguments)
+            else:
+                paybot.sendMessage(user_id, "Try /start")
+                print("POTENTIAL SQL INJECTION THREAT FROM {}!".format(user_id))
+
+    except Exception as e:
+        message_admins(e, user_id, username, message)
+        print(e)
+
+def message_admins(exception, user_id, username, message):
+    EXCEPTION_TRIGGERED_MESSAGE = "Dear admin of @paybobbot, an exception has been triggered by {}. The exception message is: {}. "
+    EXCEPTION_CAUSING_MESSAGE = "The message which cause the exception was: {}".format(message)
+    admin_ids = [229191458, 197340571]
+    user = username + ": " + str(user_id)
+    for id in admin_ids:
+        paybot.sendMessage(id, EXCEPTION_TRIGGERED_MESSAGE.format(user, str(exception)) + EXCEPTION_CAUSING_MESSAGE)
