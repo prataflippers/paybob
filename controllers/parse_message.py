be1f9438b836bb660297d1d596e3868b731cc886
Suyash Shekhar <suyashshekhar@gmail.com>
2017-10-16 15:55:21 +0800

    Only allow authorised users

--- controllers/parse_message.py
+++ controllers/parse_message.py
@@ -5,12 +5,13 @@ from history_handler import history_handler
 from loan_handler import loan_handler
 from give_handler import give_handler
 from retract_handler import retract_handler
+from start_handler import start_handler
 import telepot
 import Database
 
 db = Database.Database()
 
-def parse_handler(user_id, message):
+def parse_handler(user_id, username, message):
     '''
     Passes on the user request to the specific controller
     ARGUMENTS:
@@ -21,19 +22,28 @@ def parse_handler(user_id, message):
     paybot = telepot.Bot("452146569:AAFd8H6aj0ifJIpVT_zfxlSad8WOBUSjU2c")
     command = message.split(' ')[0][1:]
     arguments = message.split(' ')[1:] if len(message.split(' ')) > 1 else None
-    if command == "acknowledge":
-        acknowledge_handler(user_id, arguments)
-    elif command == "add":
-        add_handler(user_id, arguments)
-    elif command == "debts":
-        debt_handler(user_id, arguments)
-    elif command == "history":
-        history_handler(user_id, arguments)
-    elif command == "loans":
-        loan_handler(user_id, arguments)
-    elif command == "give":
-        give_handler(user_id, arguments)
-    elif command == "retract":
-        retract_handler(user_id, arguments)
+
+    if db.userExists(user_id):
+        if command == "start":
+            start_handler(user_id, arguments)
+        elif command == "acknowledge":
+            acknowledge_handler(user_id, arguments)
+        elif command == "add":
+            add_handler(user_id, arguments)
+        elif command == "debts":
+            debt_handler(user_id, arguments)
+        elif command == "history":
+            history_handler(user_id, arguments)
+        elif command == "loans":
+            loan_handler(user_id, arguments)
+        elif command == "give":
+            give_handler(user_id, arguments)
+        elif command == "retract":
+            retract_handler(user_id, arguments)
+        else:
+            paybot.sendMessage(user_id, "Invalid Command")
     else:
-        paybot.sendMessage(user_id, "Invalid Command")
+        if command == "start":
+            start_handler(user_id, username, arguments)
+        else:
+            print("POTENTIAL SQL INJECTION THREAT FROM {}!".format(user_id))
