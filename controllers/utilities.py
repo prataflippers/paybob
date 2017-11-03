ca8428fc23c248728c3963393e40665115c39bca
Suyash Shekhar <suyashshekhar@gmail.com>
2017-11-04 00:12:29 +0800

    Add validation check functions

--- controllers/utilities.py
+++ controllers/utilities.py
@@ -27,6 +27,38 @@ def getAbsoluteAmount(transactionAmount):
   return math.fabs(transactionAmount)
 
 '''
+ checks if a String represents a Fractional or Integral
+'''
+def isNumber(str):
+    if (str[0] == '.' or str[len(str) - 1] == '.'):
+        return False
+    foundFloatingPoint = False
+    for digit in str:
+        if not digit.isdigit():
+            if (digit == '.'):
+                if (foundFloatingPoint):
+                    return False
+                else:
+                    foundFloatingPoint = True
+            else:
+                return False
+    return True
+
+'''
+ accepted characters: A-z (case-insensitive), 0-9 and underscores.
+ length: 5-32 characters.
+'''
+def isValidTelegramUsername(string):
+    length = len(string)
+    validLength = length >= 5 and length <= 32
+    if validLength:
+        for char in string:
+            if not(char.isalpha() or char.isdigit() or char == '_'):
+                return False
+        return True
+    else:
+        return False
+'''
   tests
 '''
 def main():
