f7bfac0d08b78bda82c5054cb7cc393b4bb9114e
Suyash Shekhar <suyashshekhar@gmail.com>
2018-09-18 17:47:14 +0800

    Disable admin logging 2

--- controllers/parse_message.py
+++ controllers/parse_message.py
@@ -48,5 +48,6 @@ def timeInTimezone(timezone_name):
     return zone_time.strftime('%H:%m hours on %d/%m/%Y')
 
 def message_admins(exception, user_id, username, message):
+    print(message)
     # user = username + ": " + str(user_id)
     # logger.notify_admins(EXCEPTION_TRIGGERED_MESSAGE.format(user, str(exception)) + EXCEPTION_CAUSING_MESSAGE)
